4a9f14dd708bc25937c4cf8b6f69119a78849f65
tkit <ch1994@outlook.com>
2018-04-24 13:40:23 +0800

    add: a script to create question

---
+++ create_question.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+QUESTION_NAME=$1
+mkdir -p "src/${QUESTION_NAME}/python"
+touch "src/${QUESTION_NAME}/python/solution.py"
+touch "src/${QUESTION_NAME}/python/solution_test.py"
+touch "src/${QUESTION_NAME}/question.md"
