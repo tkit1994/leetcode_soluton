c6392327c3d1acb5e48f0bbf18fe97fbeaaa2d9b
tkit <ch1994@outlook.com>
2018-04-24 13:47:57 +0800

    update: create_question.sh

--- create_question.sh
+++ create_question.sh
@@ -4,3 +4,11 @@ mkdir -p "src/${QUESTION_NAME}/python"
 touch "src/${QUESTION_NAME}/python/solution.py"
 touch "src/${QUESTION_NAME}/python/solution_test.py"
 touch "src/${QUESTION_NAME}/question.md"
+
+cat > "src/${QUESTION_NAME}/question.md" << EOL
+# ${QUESTION_NAME}
+
+## Description
+
+## Example
+EOL
